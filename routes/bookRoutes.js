29ad915ba2c58cadc3eab4c8800a730a6afcb6ce
4drea4 <bakerand@outlook.com>
2026-01-16 21:14:25 -0600

    setting up for task 4

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -1,2 +1,29 @@
 const express = require('express');
 const Book = require('../models/Book');
+
+const router = express.Router();
+
+
+//post
+router.post('/', async (req,res) => {
+    try{
+
+    }catch {
+
+    }
+}
+
+//get /
+
+
+// get /id:
+
+
+// PUT /:id
+
+
+//delete
+
+)
+
+//export the router

ca472fc9a4424f5e6978ee9793f91e6732b6914e
4drea4 <bakerand@outlook.com>
2026-01-16 20:50:23 -0600

    got server to run, also forgot to download nodemon

--- server.js
+++ server.js
@@ -6,3 +6,12 @@ const PORT = process.env.PORT || 3001;
 
 //adding middleware
 app.use(express.json());
+
+//route to test server
+app.get('/', (req,res) => {
+    res.send('<h1>Digital Bookshelf</>');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`)
+})

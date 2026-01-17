9b27d7cd2234fd790619dc02e1bb793f4a4a7d91
4drea4 <bakerand@outlook.com>
2026-01-16 20:57:54 -0600

    added to connection.js

--- db/connection.js
+++ db/connection.js
@@ -1,1 +1,19 @@
 const mongoose = require('mongoose');
+
+const connectToDB = async () =>{
+    const mongoUri= process.env.MONGODB_URI;
+
+    if(!mongoUri) {
+        console.error('MONGODB_URI is not in .env');
+        return;
+    }
+
+    try{
+        await mongoose.connect(mongoUri);
+        console.log('Connected successfully to MongoDB');
+    } catch (error) {
+        console.error('MongoDB is failing', error.message);
+        return;
+    }
+}
+module.exports = connectToDB;

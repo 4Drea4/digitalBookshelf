26dac7f242d6b8d76a09fd5ee2dbf3337f48d23d
4drea4 <bakerand@outlook.com>
2026-01-16 21:06:02 -0600

    updated validation schema to books

--- models/Book.js
+++ models/Book.js
@@ -1,1 +1,31 @@
 const mongoose = require('mongoose');
+
+//add my validation and schema here
+const bookSchema = new mongoose.Schema (
+
+    {
+        title: {
+            type: String,
+            required: [true, 'Enter your title'],
+        },
+        author: {
+            type: String,
+            required: true
+        },
+        isbn:{
+            type: String,
+            unique:true
+        },
+        publishedDate: {
+            type:Date
+        },
+        inStock:{
+            type:Boolean,
+            default:true
+        }
+    })
+
+    const Book = mongoose.model('Book', bookSchema);
+
+    module.exports = Book;
+    

dff783d3bd83d59529830ca4f451d82e248961ab
4drea4 <bakerand@outlook.com>
2026-01-16 21:54:55 -0600

    basically done i think

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3001;
 const connectToDB = require('./db/connection');
-
+const bookRoutes = require('express');
 //adding middleware
 app.use(express.json());
 
